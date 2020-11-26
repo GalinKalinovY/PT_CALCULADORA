087afc0a9a70d3094867c4fae1299503d594fbe2
GalinKalinovY <gyordanov@almata.cat>
2020-11-26 19:09:05 +0100

    Tenim el control de erorrs fet, nomes ens falte acabar de fer el resoldre().

--- JS/calc_normal.js
+++ JS/calc_normal.js
@@ -1,9 +1,10 @@
 //var maximCaracters = [];//arraylist on tindrem els valors clicats
 
 var operadors = [];//arraylist on tindrem els valors clicats
-var operacions =[];
+var operacions =[];//arraylist on tinrem les operacions
 var operadorActual="";
 const max_length = 8;
+var operacioCompleta = concat(operadors, operacions);
 
 function dis(val){
   //el parametre val aporta el valor que hem clicat en cada boto.
@@ -15,25 +16,90 @@ function dis(val){
 //controlar numero punts 50. 50.64
 
     if (operadorActual.length < max_length) { //si hi ha mes de 5 caracters
-
-      operadorActual += val;
-      document.getElementById("resultat").value = operadorActual;
-
-      //clr();
+          operadorActual += val;
+          document.getElementById("resultat").value = operadorActual;
     }else{
-      //alert("Error el numero maxim de caracters que pots introduïr es de 5");
-      console.log("Eoror numero massa llarg");
+      alert("El numero maxim de caracters que pots introduïr es de 5");
+      console.log("Error numero massa llarg.");
     }
 
+
+}
+function disPunt(val){
+
+        let res = operadorActual.match(/\./g);
+        console.log("El numero de punts: ",res);
+        if(res === null){
+          operadorActual += val;
+          document.getElementById("resultat").value = operadorActual;
+        }else{
+            console.log("Error mes de 1 punt.");
+        }
 }
 
 function operacio(val){
-  //el parametre val aporta el valor que hem clicat en cada boto.
-  //document.getElementById("resultat").value+=val
-  operador.push(operadorActual);
-  operacions.push(valor);
-  operadorActual="";
+    console.log(" Operacions abans", operadors.length, operacions.length);
+
+if ( operadors.length  == (operacions.length +1) ){
+    console.log("operacions despres", operadors.length, operacions.length);
+    console.log("error massa operacions");
+}else{
+
+  if (operadorActual != "") {
+    operadors.push(operadorActual);
+    operacions.push(val);
+  }else{
+    console.log("operadorActual 0 ",operadorActual);
   }
+  //operadors.push(operadorActual);
+
+  operadorActual="";
+  console.log("hem clicat operacio", operadors, " ,,",operacions, " ,,",operadorActual);
+}
+  }
+
+// funcio mira el valor que hem clicat i retorna el resultat.
+function resoldre(){
+
+        let i = 0;
+        let j = 0;
+        let resultatTemp;
+        while(operadors[i] && operacions[j] !=""){
+            for(let x=0;x < operadors.length; x++){
+              console.log("for de operadors ",operadors);
+            }
+              for(let x1=0; x1 < operacions.length; x1++){
+                console.log("for de operadors ",operacions);
+
+              }
+
+            i += 1;
+            j += 1;
+        }
+
+
+
+        /*let x = operadorActual;
+        let y = eval(x);
+        document.getElementById("resultat").value = y*/
+}
+
+function dividir(val) {//agafe tots els numeros anteriors al signe de dividir.
+  //Comprobar longitud
+  operadorActual += val;
+  operadorActual.lengh < max_length;
+  console.log("estem a la funcio dividir",operadorActual);
+  document.getElementById("resultat").value = operadorActual;
+}
+
+//neteijem la pantalla de la calculadora.
+function clr(){
+        document.getElementById("resultat").value = ""
+        operadorActual="";
+        operadors = []
+        operacions = [];
+}
+
 
 
 
@@ -55,28 +121,4 @@ function operacio(val){
       }
 
   }
-
-
-
-
 */
-
-
-
-
-
-
-
-
-
-// funcio mira el valor que hem clicat i retorna el resultat.
-function resoldre(){
-        let x = document.getElementById("resultat").value
-        let y = eval(x)
-        document.getElementById("resultat").value = y
-}
-
-//neteijem la pantalla de la calculadora.
-function clr(){
-        document.getElementById("resultat").value = ""
-}
